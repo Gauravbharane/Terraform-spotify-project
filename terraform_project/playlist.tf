464bb954666ed0fcb68d896a9045d3bf647efc46
Gaurav Bharane <64822461+Gauravbharane@users.noreply.github.com>
2025-05-21 08:37:41 +0530

    Add files via upload

---
+++ terraform_project/playlist.tf
@@ -0,0 +1,6 @@
+resource "spotify_playlist" "SigmaSongs"{
+    name = "Sigmasongs"
+    tracks = [
+                "72Z2D7jpKevicRkyL45mbw"
+             ]
+}
---
+++ terraform_project/terraform.tfvars
@@ -0,0 +1,1 @@
+api_key = "rabMS7CN0EKWhoXDXaLE3Z7xT7tTc_CZEmsTkEKhmS4vWAawDiiDl8VkibHGwmuO"
